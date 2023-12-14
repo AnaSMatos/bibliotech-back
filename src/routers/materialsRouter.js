52ae8b1b0a03573127d45bd53eb741ca41dd7867
Ana Clara Matos <ana.matos@agrisafe.agr.br>
2023-12-14 14:48:44 -0300

    add delete and update functions to materials

--- src/routers/materialsRouter.js
+++ src/routers/materialsRouter.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
-import { registerMaterial, getMaterials } from '../controllers/materialsController.js';
+import { registerMaterial, getMaterials, deleteMaterial, updateMaterial } from '../controllers/materialsController.js';
+import tokenValidator from '../middlewares/tokenValidator.js';
 
 const materialsRouter = Router();
 
 materialsRouter.get('/materials', getMaterials);
-materialsRouter.post('/materials', registerMaterial);
+materialsRouter.post('/materials', tokenValidator,registerMaterial);
+materialsRouter.put('/materials', tokenValidator, updateMaterial);
+materialsRouter.delete('/materials', tokenValidator, deleteMaterial);
 
 export default materialsRouter;
