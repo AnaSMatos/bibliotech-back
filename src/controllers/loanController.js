58b660adba5204c9a8f4cf5e97cb909887edabba
Ana Clara Matos <ana.matos@agrisafe.agr.br>
2023-12-14 12:17:00 -0300

    feat: add borrow books logic

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -3,11 +3,24 @@ import { getFormattedDate } from '../utils/getFormattedDate.js';
 
 export async function borrowItem(req, res){
     try{
-        const {id_usuario, id_item, data_devolucao_prevista} = req.body;
+        const {id_usuario, id_livro, id_material_didatico, data_devolucao_prevista, tipo_item} = req.body;
         const data_emprestimo = getFormattedDate()
         await db.query(`
-        INSERT INTO emprestimos (id_usuario, id_item, data_emprestimo, data_devolucao_prevista, status) 
-	    VALUES ($1, $2, $3, $4, $5)`, [id_usuario, id_item, data_emprestimo, data_devolucao_prevista, 'Em andamento']);
+        INSERT INTO emprestimos (id_usuario, id_livro, id_material_didatico, data_emprestimo, data_devolucao_prevista, tipo_item, status) 
+	    VALUES ($1, $2, $3, $4, $5, $6, $7)`, [id_usuario, id_livro, id_material_didatico, data_emprestimo, data_devolucao_prevista, tipo_item, 'Em andamento']);
+        if(id_livro){
+            await db.query(`
+            UPDATE livros
+            SET is_available = false
+            WHERE "ISBN" = $1
+            `, [id_livro])
+        }else{
+            await db.query(`
+            UPDATE materiais_didaticos
+            SET is_available = false
+            WHERE id = $1
+            `, [id_material_didatico])
+        }
         res.sendStatus(201)
     }catch(err){
         console.log(err)
@@ -17,12 +30,26 @@ export async function borrowItem(req, res){
 
 export async function returnItem(req, res){
     try{
-        const {id_emprestimo} = req.body
+        const {id_emprestimo, id_livro, id_material_didatico} = req.body
         await db.query(`
             UPDATE emprestimos
             SET status = $1
             WHERE id = $2
         `, ['Concluído', id_emprestimo])
+
+        if(id_livro){
+            await db.query(`
+            UPDATE livros
+            SET is_available = true
+            WHERE "ISBN" = $1
+            `, [id_livro])
+        }else{
+            await db.query(`
+            UPDATE materiais_didaticos
+            SET is_available = true
+            WHERE id = $1
+            `, [id_material_didatico])
+        }
         res.sendStatus(200)
     }catch(err){
         console.log(err)
